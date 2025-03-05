aebd4d0bf4dcfad794750c29f7ea53954951a8e1
Zachary K. <zkesler2027@gmail.com>
2025-03-05 17:36:49 +0000

    Finished

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 # 5.01 Internet Slang
 
-internet_slang = {}
+internet_slang = {
+    'nbd': 'no big deal', 
+    'imo': 'in my opinion',
+    'btw': 'by the way',
+    'smh': 'shaking my head',
+    'tbh': 'to be honest',
+    'idk': "I don't know"
+}
+for trm in internet_slang:
+    print(f"{trm} means {internet_slang[trm]}.")
+while True:
+    term = input("What abbreviation would you like to look up? ")
 
+    if term in internet_slang:
+        print(f'{term} means {internet_slang[term]}.')
+    else:
+        print("That isn't a term I know.")
+        add_to = input("Would you like to add it? (Y/N) ")
+        if add_to.lower() == 'y':
+            # add term to dictionary
+            definition = input(f'What does {term} mean? ')
+            internet_slang[term] = definition
+        else:
+            print("Goodbye")
